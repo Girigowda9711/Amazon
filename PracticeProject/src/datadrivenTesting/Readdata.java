21db196d727bdcbe983ef98e7117ef5d53f49b80
Girigowda9711 <girigowda9711@gmail.com>
2022-12-07 21:36:18 +0530

    first commit

---
+++ PracticeProject/src/datadrivenTesting/Readdata.java
@@ -0,0 +1,11 @@
+package datadrivenTesting;
+
+import java.io.File;
+
+public class Readdata {
+	public static void main(String[] args) {
+		File file =new File(System.getProperty("user.dir")+"Testdata\\testdata.xlsx");
+	}
+	
+
+}
